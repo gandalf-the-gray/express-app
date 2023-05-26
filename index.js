30958f620120051ba1c2a1c0190b330da5eea082
Nitesh Yadav <nitesh9501@hotmail.com>
2023-05-26 17:20:15 +0530

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import http from "http";
 const server = http.createServer((req, res) => { res.end('It works') });
-server.listen(9000, '0.0.0.0', (err) => {
-    console.log(err ? err : `Listening to ${9000}`);
+server.listen(3000, '0.0.0.0', (err) => {
+    console.log(err ? err : `Listening to ${3000}`);
 });
